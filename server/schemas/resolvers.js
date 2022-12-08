dff21ab7abab11a5066e4dbf1b40522dbeb6cc38
JAG <jagrasso42@gmail.com>
2022-12-07 19:03:21 -0500

    add resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,67 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Thought } = require('../models');
+const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
-const resolvers = {};
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+        return userData;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  },
+
+  Mutation: {
+    signup: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+
+    login: async (parent, { username, email, password }) => {
+      const user = await User.findOne({ $or: [{ username }, { email }] });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    saveBook: async (parent, args, context) => {
+      if (!context.user) {
+        throw new AuthenticationError('You must be logged in to preform this action');
+      }
+      const updatedUser = await User.findByIdAndUpdate(
+        { _id: context.user._id },
+        { $addToSet: { savedBooks: args } },
+        { new: true, runValidators: true }
+      );
+      return updatedUser;
+    },
+
+    deleteBook: async (parent, args, context) => {
+      if (!context.user) {
+        throw new AuthenticationError('You must be logged in to preform this action');
+      }
+      const updatedUser = await User.findByIdAndUpdate(
+        { _id: context.user._id },
+        { $pull: { savedBooks: { bookId: args.bookId } } },
+        { new: true }
+      );
+      return updatedUser;
+    },
+  },
+};
 
 module.exports = resolvers;
